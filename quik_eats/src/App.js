1da50128d34714096bc7d635c331fe7ab19c1437
ishanirathnayaka <ishanirathnayaka00@gmail.com>
2025-03-04 23:08:39 +0530

    Update App.js

--- quik_eats/src/App.js
+++ quik_eats/src/App.js
@@ -1,16 +1,22 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home'; 
+import Footer from './Components/Footer';
 
- 
 function App() {
   return (
-   <div>
-    <Router>
-        <Navbar/>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          
+        </Routes>
+        <Footer />
       </Router>
-   </div>
+    </div>
   );
 }
 
