478a7a9180b026211871970da2080fa423f87e4e
ishanirathnayaka <ishanirathnayaka00@gmail.com>
2025-03-06 23:12:50 +0530

    Create Data.js

---
+++ quik_eats/src/Data/Data.js
@@ -0,0 +1,43 @@
+import RiceCurry from "../Assests/rice-and-curry.png";
+import hoppers from "../Assests/hopper.png";
+import roti from "../Assests/roti.png";
+import noodles from "../Assests/noodles.png";
+import kurakkan from "../Assests/kurakkan.png";
+import StringHoppers from "../Assests/s-hoppers.png";
+
+
+export const Data = [
+    {
+    name: "Rice & Curry",
+    image:RiceCurry,
+    price: 250,
+},
+{
+    name: "Hoppers",
+    image:hoppers,
+    price: 150,
+},
+
+{
+    name: "Roti",
+    image:roti,
+    price: 200,
+},
+{
+    name: "Noodles",
+    image:noodles,
+    price: 300,
+},
+{
+    name: "Kurakkan thalapa",
+    image:kurakkan,
+    price: 350,
+},
+{
+    name: "String Hoppers",
+    image:StringHoppers,
+    price: 350,
+},
+
+
+];
