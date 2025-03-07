63798b097c559d77a734715a99cb0d6a7d678361
ishanirathnayaka <ishanirathnayaka00@gmail.com>
2025-03-07 23:32:39 +0530

    Update App.js

--- quik_eats/src/App.js
+++ quik_eats/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home'; 
 import Footer from './Components/Footer';
 import Menu from './Pages/Menu';
+import About from './Pages/About';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' exact element={<Home />} />
           <Route path='/menu' element={<Menu />} />
+          <Route path='/about' element={<About />} />
           
         </Routes>
         <Footer />
